acf9bcbff03f4eb11450a3f676e3c08e53c1ef97
ITimon <ITimon@users.noreply.github.com>
2020-07-23 18:49:38 +0300

    StatisticsProcessor&Manager Plan
    План работы логики сбора статистики

---
+++ Core/Source/Logic/StatisticsManager.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Source.Logic
+{
+    /// <summary>
+    /// Управляет процессом сбора статистики, устанавливает интервал, запускает и приостанавливает процесс.
+    /// </summary>
+    public class StatisticsManager
+    {
+
+    }
+}
